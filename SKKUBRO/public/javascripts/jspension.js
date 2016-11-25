f5a3947f8b0b5702338ea9cfdaa143fee27f56e4
SungWoo Park <SungWoo Park>
2016-11-25 22:01:20 +0900

    펜션 업글중 제이슨으로 변경

--- SKKUBRO/public/javascripts/jspension.js
+++ SKKUBRO/public/javascripts/jspension.js
@@ -1,3 +1,19 @@
-$(function(){
-
+$(function() {
+    $('#counsel_btn').on('click', function(event) {
+        event.preventDefault();
+        var pensionOptionsStr = '{';
+        $('input[type="checkbox"]').each(function(index) {
+            console.log($(this).attr('pension'));
+            if ($(this).is(':checked')) {
+                pensionOptionsStr += '"' + $(this).attr('pension') + '"' + ':true,';
+            } else {
+                pensionOptionsStr += '"' + $(this).attr('pension') + '"' + ':false,';
+            }
+        });
+        console.log(pensionOptionsStr);
+        pensionOptionsStr = pensionOptionsStr.substring(0, pensionOptionsStr.length-1);
+        pensionOptionsStr+='}';
+        console.log(JSON.parse(pensionOptionsStr));
+        console.log(JSON.parse(pensionOptionsStr).픽업);
+    });
 });
